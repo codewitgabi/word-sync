4f13f220acedee7412493a83bd535b5122b2d8ca
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2025-09-12 01:41:31 +0100

    chore: add typing to auth controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,13 @@
+import type { Request, Response } from "express";
 import authService from "../services/auth.service";
 import catchAsync from "../utils/catch-async";
 
-export const registerUser = catchAsync(async (req, res) => {
+export const registerUser = catchAsync(async (req: Request, res: Response) => {
   const response = await authService.registerUser(req.body);
   return res.status(response.httpStatus).json(response);
 });
 
-export const login = catchAsync(async (req, res) => {
+export const login = catchAsync(async (req: Request, res: Response) => {
   const response = await authService.login(req.body);
   return res.status(response.httpStatus).json(response);
 });
